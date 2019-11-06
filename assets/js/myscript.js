d2ea7896d1c3e2ebbe0ca2c8e097d3c424de0985
vonaxl <vonaxlcorso@gmail.com>
2019-11-06 16:09:58 +0100

    puuushhh

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,18 +2,19 @@ var x=0, y=0;
 $( document ).ready(function() {
 
     $(".box").click(function(){
-        if ($(this).hasClass("red")) {
-            $(this).css("background", "red")
+        if ($(this).hasClass("red") && !$(this).hasClass("clicked")) {
+            $(this).addClass("clicked");
+            $(this).css("background", "red");
             x++;
             document.getElementById('scoreRed').innerHTML = x;
             console.log(x);
             
-        }else{
-            $(this).css("background", "green")
+        }else if (!$(this).hasClass("clicked")) {
+            $(this).addClass("clicked");
+            $(this).css("background", "green");
             y++;
             document.getElementById('scoreGreen').innerHTML = y;
         }
     });
-
 });
 
