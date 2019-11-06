9b2e824c17dc1cd49aabc5ab6fabb254a5650946
vonaxl <vonaxlcorso@gmail.com>
2019-11-06 13:54:43 +0100

    push

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,10 +1,17 @@
-
-
+var x=0;
 $( document ).ready(function() {
 
     $(".box").click(function(){
-        $(this).css("background", "red");
+        if ($(this).hasClass("red")) {
+            $(this).css("background", "red")
+            x++;
+            document.getElementById('score').innerHTML = x;
+            console.log(x);
+            
+        }else{
+            $(this).css("background", "green")
+        }
     });
 
-
 });
+
