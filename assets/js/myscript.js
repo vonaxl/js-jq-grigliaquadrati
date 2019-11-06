ad54db2d5e959a3acfeab122f93c08a07bb3ee07
vonaxl <vonaxlcorso@gmail.com>
2019-11-06 13:59:09 +0100

    PUUSH

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,15 +1,17 @@
-var x=0;
+var x=0, y=0;
 $( document ).ready(function() {
 
     $(".box").click(function(){
         if ($(this).hasClass("red")) {
             $(this).css("background", "red")
             x++;
-            document.getElementById('score').innerHTML = x;
+            document.getElementById('scoreRed').innerHTML = x;
             console.log(x);
             
         }else{
             $(this).css("background", "green")
+            y++;
+            document.getElementById('scoreGreen').innerHTML = y;
         }
     });
 
